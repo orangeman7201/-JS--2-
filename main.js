3937ad305b978bc492780062cc51c3e8c8f704db
orangeman7201 <aki.jazz1027@gmail.com>
2021-05-31 14:18:17 +0900

    JS課題②

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
 'use strict';
 
-const todos = {
-  掃除:'家事',
-  洗濯:'家事'
+function addList (sum) {
+    const li = document.createElement('li')
+    li.textContent = sum;
+    document.querySelector('ul').appendChild(li);
 };
 
-function addTodo () {
-  let task = prompt ('タスクを入力してください!');
-  let genre = prompt ('ジャンルを入力してください!');
-  alert ('タスクを追加しました');
-  todos[task] = genre;
-};
-
-function showTodo () {
-  console.log ('==========\nタスク一覧\n==========');
-  const keys = Object.keys(todos);
-  keys.forEach((key, index) => {
-    console.log(`${index}: [内容]: ${key} [ジャンル]: ${todos[key]}`)
-  });
+function multipleCalculator (a, b) {
+  let aSum = a;
+  let bSum = b;   
+  while (aSum < 100) {
+    if (aSum < bSum){
+      addList(`Fizz${aSum}`);
+      aSum += a;
+    }else if (aSum > bSum ) {
+      addList(`Buzz${bSum}`);
+      bSum += b;
+    }else {
+      addList(`FizzBuzz${aSum}`);
+      aSum += a;
+      bSum += b;
+    }
+  }; 
 }
 
+document.querySelector('button').addEventListener('click', () => {
 
-showTodo ();
-addTodo ();
-showTodo ();
+    let ul = document.querySelector('ul');
+    while(ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    };
+
+  let x = parseInt(document.querySelectorAll('input')[0].value);
+  let y = parseInt(document.querySelectorAll('input')[1].value);
+  
+  if (Number.isInteger(x) && Number.isInteger(y)) {
+    multipleCalculator (x, y);
+  }else{
+    addList("整数を入力してください");
+  }
+  
+});
