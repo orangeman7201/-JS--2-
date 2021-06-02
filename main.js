a88f14ac4575d4efc7c285c14c6aa1c4456dc013
orangeman7201 <aki.jazz1027@gmail.com>
2021-06-03 00:05:07 +0900

    JS課題②不明箇所あり

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 'use strict';
 
-function addList (sum) {
+function addList(sum) {
   const li = document.createElement('li')
   li.textContent = sum;
   document.querySelector('ul').appendChild(li);
 };
 
-function multipleCalculator (a, b) {
+function multipleCalculator(a, b) {
   let aSum = a;
   let bSum = b;   
   while (aSum < 100) {
-    if (aSum < bSum){
+    if (aSum < bSum) {
       addList(`Fizz${aSum}`);
       aSum += a;
-    }else if (aSum > bSum ) {
+    } else if (aSum > bSum) {
       addList(`Buzz${bSum}`);
       bSum += b;
-    }else {
+    } else {
       addList(`FizzBuzz${aSum}`);
       aSum += a;
       bSum += b;
@@ -24,20 +24,27 @@ function multipleCalculator (a, b) {
   }; 
 }
 
+function isNumber(number) {
+   let regex = new RegExp(/^[0-9]+(\.[0-9]+)?$/);
+  return regex.test(number);
+};
+
 document.querySelector('button').addEventListener('click', () => {
 
-  let ul = document.querySelector('ul');
-  while(ul.firstChild) {
+  const ul = document.querySelector('ul');
+  while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
   };
 
-  let x = parseInt(document.querySelectorAll('input')[0].value);
-  let y = parseInt(document.querySelectorAll('input')[1].value);
+  const fizzInputNumber = parseFloat(document.getElementById('fizzInputField').value);
+  const buzzInputNumber = parseFloat(document.getElementById('buzzInputField').value);
+
+  // console.log(isNumber(fizzInputNumber));
   
-  if (Number.isInteger(x) && Number.isInteger(y)) {
-    multipleCalculator (x, y);
-  }else{
-    addList("整数を入力してください");
+  if (Number.isInteger(fizzInputNumber) && Number.isInteger(buzzInputNumber) && fizzInputNumber !== '' && buzzInputNumber !== '') {
+    multipleCalculator(fizzInputNumber, buzzInputNumber);
+  } else {
+    addList('整数を入力してください');
   }
   
 });
