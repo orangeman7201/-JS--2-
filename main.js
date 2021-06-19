0cec2aa282951d0d19ceccc4f8cce93791735b6d
unknown <aki.jazz1027@gmail.com>
2021-06-19 16:42:40 +0900

    JS課題③-3

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ const todos = [];
 const ul = document.querySelector('ul');
 let statusButton,deleteButton;
 
+
 function addTodo (word) {
   todos.push(word);
 }
+
 
 function showTodos () {
   todos.forEach((todo, index) => {
@@ -16,30 +18,46 @@ function showTodos () {
     const span = document.createElement('span');
     span.innerHTML = todo;
     li.appendChild(span);
-  
+    
     createDeleteButton ();
     addDeleteFeature (index);
     li.appendChild(deleteButton);
-
+    
     createStatusButton ();
+    addStatusFeature (index);
     li.appendChild(statusButton);
     
     ul.appendChild(li);    
   });
 };
 
+
 function createStatusButton () {
-	statusButton = document.createElement('button');
+  statusButton = document.createElement('button');
 	statusButton.textContent = '作業中';
-	statusButton.className = 'statusClassName';
+  statusButton.className = 'working';
+}
+
+
+function addStatusFeature (index) {
+  statusButton.addEventListener('click', () => {
+    const working = document.getElementsByClassName('working')[index]
+    working.classList.toggle('finish');
+    if (working.classList.contains('finish')) {
+      working.innerHTML = '完了';
+    } else {
+      working.innerHTML = `作業中`;
+    }
+  });
 }
 
 
 function createDeleteButton () {
-	deleteButton = document.createElement('button');
+  deleteButton = document.createElement('button');
 	deleteButton.textContent = '削除';
 	deleteButton.className = 'deleteClassName';
 }
+
 
 function addDeleteFeature (number) {
   deleteButton.addEventListener('click', () => {
@@ -47,14 +65,15 @@ function addDeleteFeature (number) {
     updateStatus ();
     showTodos ();
   });
-
 }
+
 
 function updateStatus () {
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
   };
 }
+
 
 document.getElementById('submit-button').addEventListener('click', () => {
   updateStatus ();
