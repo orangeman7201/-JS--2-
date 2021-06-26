1a915f7504ea367f8214c0da9ee6f578e26b8253
unknown <aki.jazz1027@gmail.com>
2021-06-26 11:54:38 +0900

    JS課題③-4

--- main.js
+++ main.js
@@ -2,95 +2,133 @@
 
 const todos = [];
 const ul = document.querySelector('ul');
+const table = document.querySelector('table');
 let statusButton,deleteButton;
 
 
-function addTodo (word) {
+function addTodo(word) {
   const addItem = {};
   addItem[word] = 'working';
   todos.push(addItem) ;
 }
 
+function createTd(todo, index) {
+  const tr = document.createElement('tr');
+  const td1 = document.createElement('td');
+  td1.innerHTML = index;
+  tr.appendChild(td1);
+  
+  const td2 = document.createElement('td');
+  td2.innerHTML = Object.keys(todo);
+  tr.appendChild(td2);
+  
+  createDeleteButton();
+  addDeleteFeature(index);
+  tr.appendChild(deleteButton);
+  
+  createStatusButton(Object.values(todo));
+  addStatusFeature(Object.keys(todo), index);
+  tr.appendChild(statusButton);
 
-function showTodos () {
+  table.appendChild(tr);
+}
+
+function showFinish() {
+  updateStatus();
   todos.forEach((todo, index) => {
-    const li = document.createElement('li');
-    li.innerHTML = index;
-    
-    const span = document.createElement('span');
-    span.innerHTML = Object.keys(todo);
-    li.appendChild(span);
-    
-    createDeleteButton ();
-    addDeleteFeature (index);
-    li.appendChild(deleteButton);
-    
-    createStatusButton (Object.values(todo));
-    addStatusFeature (Object.keys(todo), index);
-    li.appendChild(statusButton);
-    
-    ul.appendChild(li);
-   });
-}
-
-
-function createStatusButton (status) {
-  statusButton = document.createElement('button');
-  if (status == 'working finish') {
-    statusButton.className = 'working finish';
-    statusButton.textContent = '完了';
-  } else {
-    statusButton.className = 'working';
-    statusButton.textContent = '作業中';
-  }
-}
-
-
-function addStatusFeature (todo, index) {
-  statusButton.addEventListener('click', () => {
-    const working = document.getElementsByClassName('working')[index];
-    working.classList.toggle('finish');
-    if (working.classList.contains('finish')) {
-      working.innerHTML = '完了';
-      todos[index][todo] = 'working finish';
-    } else {
-      working.innerHTML = `作業中`;
-      todos[index][todo] = 'working';
+    if (`${todos[index][Object.keys(todo)]}`.includes('finish')) {
+      createTd(todo, index);
     }
   });
 }
 
+function showWorking() {
+  updateStatus();
+  todos.forEach((todo, index) => {
+    if (todos[index][Object.keys(todo)] === 'working') {
+      createTd(todo, index);
+    }
+  });
+}
 
-function createDeleteButton () {
+function showTodos() {
+  updateStatus();
+  todos.forEach((todo, index) => {
+    createTd(todo, index);
+ });
+}
+
+function createStatusButton(status) {
+  statusButton = document.createElement('button');
+  if(status == 'working finish') {
+    statusButton.textContent = '完了';
+  } else {
+    statusButton.textContent = '作業中';
+  }
+}
+
+function addStatusFeature(todo, index) {
+  statusButton.addEventListener('click', () => {
+    if(`${todos[index][todo]}`.includes('finish')) {
+      todos[index][todo] = 'working';
+    } else {
+      todos[index][todo] = 'working finish';
+    }
+  radioFunction();
+  });
+}
+
+function createDeleteButton() {
   deleteButton = document.createElement('button');
 	deleteButton.textContent = '削除';
 	deleteButton.className = 'deleteClassName';
 }
 
-
-function addDeleteFeature (number) {
+function addDeleteFeature(number) {
   deleteButton.addEventListener('click', () => {
     todos.splice(number, 1);
     updateStatus ();
-    showTodos ();
+    radioFunction();
   });
 }
 
-
-function updateStatus () {
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild);
+function updateStatus() {
+  while (table.firstChild) {
+    table.removeChild(table.firstChild);
   };
+  const tr = document.createElement('tr');
+  tr.innerHTML = '<th>ID</th><th>コメント</th><th align="left">状態</th>';
+  table.appendChild(tr);
 }
 
+function getRadioValue(){
+  const radios = document.getElementsByName('status');
+  let result;
+  for(let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      result = radios[i].value;
+      break;
+    }
+  }
+  return result;
+}
+
+function radioFunction() {
+  if (getRadioValue() === 'all') {
+    showTodos();
+  } else if (getRadioValue() === 'working'){
+    showWorking();
+  } else if (getRadioValue() === 'working finish') {
+    showFinish();
+  }
+}
 
 document.getElementById('submit-button').addEventListener('click', () => {
   updateStatus ();
-    
   const inputJS = document.getElementById('input-html');
   let todoItem = inputJS.value;
   addTodo(todoItem);
-  showTodos();
+  radioFunction();
   inputJS.value = '';
   inputJS.focus();
 });
